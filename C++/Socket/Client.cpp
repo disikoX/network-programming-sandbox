d626ada488e9ef2752d0c4bf37b836751aaca5e3
disikoX <damien.zara12@gmail.com>
2025-03-28 01:23:33 +0300

    Add : Asking the server for a connection

--- C++/Socket/Client.cpp
+++ C++/Socket/Client.cpp
@@ -1,25 +1,36 @@
+#include <iostream>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <cstring>
+#include <unistd.h>
 
-int fd; // file descriptor
+using namespace std;
 
-fd = socket(AF_INET, SOCK_STREAM, 0);
+int main() {
 
-if (fd == -1) {
-  
-  switch (errno) {
+  int fd = socket(AF_INET, SOCK_STREAM, 0);  
+  struct sockaddr_in client_address;
 
-    case EPROTONOTSUPPORT :
-     std::cout << "Protocol not supported";
+  // Specifying the address
+  memset(&client_address, 0, sizeof(client_address));
+  client_address.sin_family = AF_INET;
+  client_address.sin_port = htons(8080);
+  client_address.sin_addr.s_addr = INADDR_ANY;
 
-    case EACESS : 
-     std::cout << "Permission denied";
-
-    default:
-      std::cout << "Check the Unix Manual";
-      break;
+  //  Ask a Connection 
+  if (connect(fd, (struct sockaddr *)&client_address, sizeof(client_address)) < 0)
+  {
+    perror("Connection failed : ");
+    close(fd);
+    return 1;
   }
+  
+  cout << "Connection successful" << endl;
 
+  close(fd);
+
+
+  return 0;
 }
-
-
